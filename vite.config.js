9228abb9999f49fac60739a701110e1562bd4f8a
Dhia Nabli <bytejutsuproject@gmail.com>
2024-09-16 13:28:49 +0100

    testing github actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/xboard/' : '',
   plugins: [react()],
   test: {
     globals: true,
